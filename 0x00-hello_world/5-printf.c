e4e91217d263788295e327987f2baeb76ffd8a1a
SherifatHaruna <sherifatharuna8@gmail.com>
2022-09-08 17:31:05 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Printmy style
+*
+*Return: Always 0 (successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
