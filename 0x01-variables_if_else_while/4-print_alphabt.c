281766f6b10adfeee3ea25d29a878773bb2577b9
SherifatHaruna <sherifatharuna8@gmail.com>
2022-09-10 05:34:18 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * *Return: 0
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+	int lw = 'a';
+
+	while (lw <= 'z')
+
+	{
+
+		if (lw == 'e' || lw == 'q')
+
+		{
+
+			lw += 1;
+
+		}
+
+		else
+
+		{
+
+			putchar(lw);
+
+			lw += 1;
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
