641be17dd1dade107456a6452866d5782d987b42
SherifatHaruna <sherifatharuna8@gmail.com>
2022-09-10 05:23:44 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * *Return: 0
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+	int l = 'a';
+
+	while (l <= 'z')
+
+	{
+
+		putchar(l);
+
+		l += 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
