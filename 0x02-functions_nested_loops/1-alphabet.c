9d94140747c7a91c3d34a9fb194ec3a6e4ffa38b
SherifatHaruna <sherifatharuna8@gmail.com>
2022-09-13 16:10:25 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,12 @@
 #include "main.h"
+
 /**
- * print_alphabet - its a function in the main header that allows us to print
+ * main - check the code for Holberton School students
  *
- * Return: Always returns 0
+ * Return: Always 0
  */
-
-void print_alphabet(void)
+int main(void)
 {
-char a = 'a';
-while (a <= 'z')
-{
-_putchar(a);
-a++;
-}
-_putchar('\n');
-
+	print_alphabet();
+	return (0);
 }
